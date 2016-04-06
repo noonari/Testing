9cfd3ebcc22c66d97881dae9366b0dcd91404139
Sarmad <SarmadSattar1@gmail.com>
2016-04-06 21:27:50 +0500

    Add project files.

---
+++ Testing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(2,3));
+        }
+
+     public static int add(int a, int b) {
+            return a + b;
+        }
+
+    }
+}
